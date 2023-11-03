7127d4f9a7adc3d4fe74a3748dffe3e3af150206
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-11-03 10:03:52 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
 const App = () => {
-  const [message, setMessage] = React.useState("메시지");
+  const [message, setMessage] = React.useState();
+  const [result, setResult] = React.useState();
 
   const handleSubmit = async (e) => {
-      console.log(message); 
+      setResult("hello "+ message);
+      console.log("hello "+ message); 
   }; 
   
   return <>
     <input value={message} onChange={(e) => { setMessage(e.target.value); }} />
     <button onClick={(e) => { handleSubmit(e); }}>Click</button>
+    <Text>{result}</Text> 
   </>;
 };
 

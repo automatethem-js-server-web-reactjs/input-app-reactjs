962bbad4285ab876d67ff118fa9ee743bb3cef20
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-11-03 09:50:07 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,136 +1,14 @@
-/*
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-/*
-import logo from './logo.svg';
-
-function App() {
-  return (
-    <>
-    <div style={styles.App}>
-      <header style={styles.AppHeader}>
-        <img src={logo} style={App.AppLogo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          style={styles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
-  );
-}
-
-export default App;
-
-const styles = {
-  App: {
-    textAlign: "center"
-  },
-  AppHeader: {
-    backgroundColor: "#282c34", 
-    minHeight: "100vh", 
-    display: "flex", 
-    flexDirection: "column", 
-    alignItems: "center", 
-    justifyContent: "center", 
-    fontSize: "calc(10px + 2vmin)", 
-    color: "white"
-  },
-  AppLogo: {
-    height: "40vmin", 
-    pointer_events: "none"
-  },
-  AppLink: {
-    color: "#61dafb"
-  }
-};
-*/
-/*
-import logo from './logo.svg';
-
-function App() {
-  return (
-    <>
-    <div style={{textAlign: "center"}}>
-      <header 
-        style={{
-          backgroundColor: "#282c34", 
-          minHeight: "100vh", 
-          display: "flex", 
-          flexDirection: "column", 
-          alignItems: "center", 
-          justifyContent: "center", 
-          fontSize: "calc(10px + 2vmin)", 
-          color: "white"
-        }}
-      >
-        <img src={logo} 
-          style={{
-            height: "40vmin", 
-            pointer_events: "none"
-          }} 
-          alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          style={{
-            color: "#61dafb"
-          }}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    </>
-  );
-}
-
-export default App;
-*/
-///*
 const App = () => {
-  document.title = "App";
+  const [message, setMessage] = React.useState("메시지");
+
+  const handleSubmit = async (e) => {
+      console.log(message); 
+  }; 
   
   return <>
-    <div>Hello world</div>
+    <input onChange={(e) => { setMessage(e.target.value); }} />
+    <button onClick={(e) => { handleSubmit(e); }}>Click</button>
   </>;
 };
 
 export default App;
-//*/
-

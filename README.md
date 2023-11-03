10bb2c940254d54953cec4d9facac07ef2e3e957
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-11-03 09:45:46 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# input-app-react
+# basic-app-react
+
+```
+//npx create-react-app basic-app-react
+//cd basic-app-react
+cd basic-app-react
+npm install
+```
+
+```
+npm start
+```
+
+![](attach_files/screenshot0.png?raw=true)
+
+![](attach_files/screenshot1.png?raw=true)
+
+![](attach_files/screenshot2.png?raw=true)
---
+++ src/App.js
@@ -0,0 +1,136 @@
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+/*
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <>
+    <div style={styles.App}>
+      <header style={styles.AppHeader}>
+        <img src={logo} style={App.AppLogo} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          style={styles.AppLink}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+const styles = {
+  App: {
+    textAlign: "center"
+  },
+  AppHeader: {
+    backgroundColor: "#282c34", 
+    minHeight: "100vh", 
+    display: "flex", 
+    flexDirection: "column", 
+    alignItems: "center", 
+    justifyContent: "center", 
+    fontSize: "calc(10px + 2vmin)", 
+    color: "white"
+  },
+  AppLogo: {
+    height: "40vmin", 
+    pointer_events: "none"
+  },
+  AppLink: {
+    color: "#61dafb"
+  }
+};
+*/
+/*
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <>
+    <div style={{textAlign: "center"}}>
+      <header 
+        style={{
+          backgroundColor: "#282c34", 
+          minHeight: "100vh", 
+          display: "flex", 
+          flexDirection: "column", 
+          alignItems: "center", 
+          justifyContent: "center", 
+          fontSize: "calc(10px + 2vmin)", 
+          color: "white"
+        }}
+      >
+        <img src={logo} 
+          style={{
+            height: "40vmin", 
+            pointer_events: "none"
+          }} 
+          alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          style={{
+            color: "#61dafb"
+          }}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
+*/
+///*
+const App = () => {
+  document.title = "App";
+  
+  return <>
+    <div>Hello world</div>
+  </>;
+};
+
+export default App;
+//*/
+
